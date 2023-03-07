2c0dc08fec29b12067db6f38789cf703d7a6c8ad
Zhuoran <lucysunzr@gmail.com>
2023-03-06 22:56:08 -0500

    Add: pages config

---
+++ readmeLoop.py
@@ -0,0 +1,16 @@
+import os
+
+def write_readme():
+    allmd = os.listdir("./docs")
+    allmd.sort(key = lambda x: int(x[3:-3]))
+
+    with open("README.md", "w") as f:
+        f.write("# GRE VOCAB SCHEDULE: \n")
+        for i in allmd:
+            f.write("* ["+ i[:-3] + "](./docs/" + i + ")\n")
+        
+        f.close()
+
+if __name__ == "__main__" :
+    write_readme()
+
